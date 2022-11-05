e3011761c982eb98cc1fc771fe451f6b1cee6098
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2022-11-05 13:40:22 +0300

    connect that to the human right, and the synthesis, peace by equality

--- src/components/subwork/Gachasphere.js
+++ src/components/subwork/Gachasphere.js
@@ -15,15 +15,19 @@ function Gachasphere() {
             fontStyle: "italic",
           }}
         >
-          Politics and Diplomacy <br></br><br></br>
+          Politics and Diplomacy <br></br>
+          <br></br>
           <span
-          style={{
-            backgroundColor: "yellow",
-            color: "black",
-            padding: "2px",
-            fontStyle: "italic",
-          }}
-        >  Case: Proactive electorship and Peacemaking </span>
+            style={{
+              backgroundColor: "yellow",
+              color: "black",
+              padding: "2px",
+              fontStyle: "italic",
+            }}
+          >
+            {" "}
+            Case: Proactive electorship for Peacemaking{" "}
+          </span>
         </span>
       </h3>
       {/* <h4 className="subwork_title--secondary">
@@ -33,21 +37,36 @@ function Gachasphere() {
             color: "white",
             padding: "2px",
             fontStyle: "italic",
-          }}
+          }:
         >
          
         </span>
       </h4> */}
       <p className="subwork_text" style={{ textAlign: "start" }}>
-        A voter/elector demands a change, without specifying or appealing to a
-        specific political party. At the initiation of the demand flow, voter give equal chance to all, as generous as possible. 
-        <br></br>        <br></br>
-
-        Because what they wanted in the first place, is progress in an arbitrary cause/policy, or new ones to be given capacity. <br></br><br></br> They expose and clarify their intentions in
-        single or multi levels, meaning one or two elections ahead of time, for any specific topic that they wish to see improved. <br></br>  <br></br>This is, hereby, a reflection of an opinionated perspective and worldview.
-        <br></br><br></br>
-        As an example, an individual demand, for global trust and peacemaking is
-        illustrated below:
+        Voters/electors demand a change, without specifying or appealing to a
+        specific political party. At the initiation of the demand flow, voters
+        give equal chances to all, as generously as possible.
+        <br></br> <br></br>
+        <i>
+          Assuming what citizens wanted in the first place is an observable
+          progress on an arbitrary cause/policy, or new ones to be given
+          capacity, more than which party/parties delivering that is of their
+          concern.
+        </i>
+        <br></br> <br></br>
+        In essence, they expose and clarify their intentions in single or
+        multi-levels *, meaning one or two elections ahead of time, for any
+        specific topic that they wish to see developments in a positive
+        direction.
+        <br></br> <br></br>
+        This is, hereby, an expressed expectation, for the reflections to their
+        opinionated perspective and worldview, and hence, an individual, change
+        request/proposal of a kind, with potential civic sanctions (at level II
+        and beyond).
+        <br></br>
+        <br></br>
+        As an example, an individual’s expectation and demand for global trust
+        and peacemaking is illustrated below:
         {/* In the first exposition, inclusion of change,
         thereby a token of intention is demanded, and in the second level,
         exposition of a future time decision is made, this time, binding for
@@ -57,17 +76,63 @@ function Gachasphere() {
         matters. */}
         <br></br>
         <br></br>
-        <span style={{backgroundColor: "black", color: "white"}}>I:</span> {" "}
-       I demand political parties that includes peacemaking with other member states in the United Nations to their political agendas and electoral promises. 
-      
-       <span style={{backgroundColor: "pink", color: "black", padding:"2px", fontStyle:"italic"}}>I will <u> (most) probably vote for  one of them </u> in the next election.</span>
-        <br></br><br></br>
-        <span style={{backgroundColor: "black", color: "white", }}>II:</span> {" "} 
-       If one of those parties elected in the next election, and kept their word on making observable effort and impact on that cause, then   <span style={{backgroundColor: "pink", color: "black", padding:"2px", fontStyle:"italic"}}> I will <u>definitely vote for them</u> in the following election(s). </span>
-       <br></br><br></br>
-      <h2>.. to be continued with <br></br> prediction-free advertising and the new, new media </h2>
+        <span style={{ backgroundColor: "black", color: "white" }}>I:</span> I
+        demand political parties to include "ultimate peace and trust making
+        with other member states in the United Nations", in their political
+        agendas and electoral promises
+        <span
+          style={{
+            backgroundColor: "pink",
+            color: "black",
+            padding: "2px",
+            fontStyle: "italic",
+          }}
+        >
+          {" "}
+          I will <u> (most) probably vote for one of them </u> in the first
+          election ahead.
+        </span>
+        <br></br>
+        <br></br>
+        <span style={{ backgroundColor: "black", color: "white" }}>II:</span> If
+        one of those parties elected in the next election, and kept their word
+        on making observable effort and impact on that cause, then{" "}
+        <span
+          style={{
+            backgroundColor: "pink",
+            color: "black",
+            padding: "2px",
+            fontStyle: "italic",
+          }}
+        >
+          {" "}
+          I will <u> (most) definitely vote for them (again)</u> in the
+          following election(s).{" "}
+        </span>
+        <br></br>
+        <br></br>
+        <span style={{ backgroundColor: "black", color: "white" }}>
+          III:
+        </span>{" "}
+        In the meantime, as I am a potential candidate in a future election, and
+        have the equal right to be an electoral nominee to become an executive
+        person with such signature power; I will proactively use my right to
+        co-sign and be part of a sealing of a World Peace treaty with other
+        individuals and states in the world, as they exposed/expressed their
+        intentions as well.{" "}
+        <span
+          style={{
+            backgroundColor: "pink",
+            color: "black",
+            padding: "2px",
+            fontStyle: "italic",
+          }}
+        ></span>
+        <h2>
+          .. to be continued with <br></br> prediction-free advertising and the
+          new, new media{" "}
+        </h2>
       </p>
-
     </div>
   );
 }
